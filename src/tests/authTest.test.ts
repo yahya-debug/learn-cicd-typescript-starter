b9b51cd112c3d0d1d4c022d6258db5ce5ab60e7e
yahya-debug <68112561+yahya-debug@users.noreply.github.com>
2026-07-21 13:56:26 +0300

    running unit testing

--- src/tests/authTest.test.ts
+++ src/tests/authTest.test.ts
@@ -1,16 +1,13 @@
 import { describe, expect, test } from "vitest";
 
-const person = {
-  isActive: true,
-  age: 32,
-};
+const person = undefined;
 
 describe("person", () => {
   test("person is defined", () => {
     expect(person).toBeDefined();
   });
 
-  test("is active", () => {
-    expect(person.isActive).toBeTruthy();
-  });
+  // test("is active", () => {
+  //   expect(person.isActive).toBeTruthy();
+  // });
 });
